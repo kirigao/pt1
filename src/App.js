7d4914fff080b2a61a47b4ff63d78a11098397b2
kg368 <kg368@cornell.edu>
2023-07-02 19:03:29 -0400

    1.14 anecdotes step3

--- src/App.js
+++ src/App.js
@@ -1,64 +1,69 @@
 import { useState } from 'react'
-const Statistics = (props) => {
-  if (props.good == 0 && props.neutral == 0 && props.bad == 0) {
-    return (
-      <div>
-        No feedback given
-      </div>
-    )
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+
+  )
+
+}
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(new Uint8Array(anecdotes.length))
+  const [mostVotes, setMostVotes] = useState(0)
+  const selectRandom = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+  const selectNext = () => {
+    if (selected == anecdotes.length - 1) {
+      setSelected(0)
+    }
+    else {
+      setSelected(selected + 1)
+    }
+
+  }
+  const updatePoints = (num) => {
+    const copy = [...points]
+    copy[num] += 1
+    setPoints(copy)
+    if (copy[num] > copy[mostVotes]) {
+      setMostVotes(num)
+    }
+    console.log("Point " + num + ": " + points[num])
   }
   return (
     <div>
-      <table>
-        <Display type="good" number={props.good} />
-        <Display type="neutral" number={props.neutral} />
-        <Display type="bad" number={props.bad} />
-        <Display type="all" number={props.good + props.bad + props.neutral} />
-        <Display type="average" number={(props.good - props.bad) / (props.good + props.bad + props.neutral)} />
-        <Display type="positive" number={props.good / (props.good + props.bad + props.neutral) + " %"} />
-      </table>
+      <h1>
+        Anecdote of the day
+      </h1>
+      {anecdotes[selected]}
+      <div>
+        has {points[selected]} votes
 
+      </div>
+
+      <Button handleClick={() => updatePoints(selected)} text="vote" />
+      <Button handleClick={selectNext} text="next" />
+      <h1>
+        Anecdote with the most votes
+      </h1>
+      {anecdotes[mostVotes]}
     </div>
   )
 }
-const Display = (props) => {
-  return (
-    <tr>
-      <td>
-        {props.type}
-      </td>
-      <td>
-        {props.number}
-      </td>
-    </tr>
-  )
-}
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>
-    {props.text}
-  </button>
-)
-
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-
-  return (
-    <div>
-      <h1>
-        give feedback
-      </h1>
-      <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />
-      <h1>
-        statistics
-      </h1>
-      <Statistics good={good} neutral={neutral} bad={bad} />
-    </div>
-  )
-}
 export default App
