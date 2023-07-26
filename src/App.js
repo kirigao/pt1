ebfc88f36d9af0312deaf3956ed8e3790b6fffaa
kg368 <kg368@cornell.edu>
2023-07-25 20:27:23 -0400

    render version notes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,86 @@
 import { useState, useEffect } from 'react'
-import countryServices from './services/countries'
-import Display from './components/Display'
-import axios from 'axios'
+import Note from './components/Note'
+
+import noteService from './services/notes'
 
 const App = () => {
-  const [important, setImportant] = useState(null)
-  const [value, setValue] = useState('')
-  const [countryData, setCountryData] = useState([])
-  const [allCountries, setAllCountries] = useState(null)
-  const api_key = process.env.REACT_APP_API_KEY
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
-  const showImportant = (name) => {
-    setImportant(name)
-  }
   useEffect(() => {
-    const countryList = countryServices.getAll()
-    console.log(countryList)
-    setCountryData(countryList)
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
   }, [])
-  const handleChange = (event) => {
-    console.log(event.target.value)
-    const val = event.target.value
-    setValue(val)
-    setImportant(null)
-    countryData.then(countries => {
-      const temp = countries.filter(countryObject => countryObject.name.common.toLowerCase().includes(val.toLowerCase()))
-      setAllCountries(temp)
 
-    })
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+    }
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
   }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        alert(
+          `the note '${note.content}' was already deleted from server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
 
   return (
     <div>
-      <form>
-        find countries <input value={value} onChange={handleChange} />
-        <Display countryList={allCountries} showImportant={showImportant} important={important} />
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        <ul>
+          {notesToShow.map(note =>
+            <Note
+              key={note.id}
+              note={note}
+              toggleImportance={() => toggleImportanceOf(note.id)}
+            />
+          )}
+        </ul>
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
       </form>
-
     </div>
   )
 }
-
 
 export default App
